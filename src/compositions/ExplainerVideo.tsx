e803ca3f131d2542d1651964915f21930b6fa0d2
Sean C Davis <scdavis41@gmail.com>
2022-03-15 16:02:57 -0400

    Set up base fonts and layout

---
+++ src/compositions/ExplainerVideo.tsx
@@ -0,0 +1,7 @@
+export const ExplainerVideo: React.FC<{}> = () => {
+  return (
+    <div className="video-layout bg-black text-white">
+      <p className="text-8xl">Video goes here ...</p>
+    </div>
+  )
+}
