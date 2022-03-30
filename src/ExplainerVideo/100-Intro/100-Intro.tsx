8d4fba9e812eda5a102b4bc2f35910d387a80a22
Sean C Davis <scdavis41@gmail.com>
2022-03-30 11:43:27 -0400

    New intro sequences

--- src/ExplainerVideo/100-Intro/100-Intro.tsx
+++ src/ExplainerVideo/100-Intro/100-Intro.tsx
@@ -1,3 +1,16 @@
+import { ContentlayerLogo } from "../../assets"
+
 export const Intro = () => {
-  return <p>Slide TBD</p>
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="py-24">
+        <div className="inline-flex items-center">
+          <div className="h-32 text-primary mr-10">
+            <ContentlayerLogo />
+          </div>
+          <h2 className="text-9xl font-bold">Contentlayer</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/ExplainerVideo/100-Intro/200-ModernWebStack.tsx
+++ src/ExplainerVideo/100-Intro/200-ModernWebStack.tsx
@@ -1,3 +1,75 @@
+import {
+  RemixLogo,
+  NextLogo,
+  SvelteLogo,
+  QuestionMarkIcon,
+  DatabaseIcon,
+  PagesIcon,
+  Arrow,
+} from "../../assets"
+
 export const ModernWebStack = () => {
-  return <p>Slide TBD</p>
+  return (
+    <div className="w-full h-full">
+      <div className="py-24">
+        <h2 className="text-7xl text-center font-bold">Modern Web Stack</h2>
+      </div>
+      <div className="py-32 px-24 flex items-center justify-between relative">
+        <div className="relative">
+          <div
+            className="text-4xl uppercase py-6 px-12 border transform -rotate-12 absolute rounded-xl"
+            style={{ top: "-5rem", left: "-2rem" }}
+          >
+            Sources
+          </div>
+          <div className="bg-gray flex items-center justify-between py-16 px-32 rounded-2xl">
+            <span className="h-32 block">
+              <DatabaseIcon />
+            </span>
+          </div>
+        </div>
+        <div>
+          <span
+            className="block absolute text-gray transform"
+            style={{
+              left: "24rem",
+              top: "-2rem",
+              width: "30rem",
+              transform: "rotate(-8deg)",
+            }}
+          >
+            <Arrow.RightDownArrow />
+          </span>
+
+          <span className="block h-56">
+            <QuestionMarkIcon />
+          </span>
+          <span
+            className="block absolute text-gray transform"
+            style={{
+              left: "60rem",
+              bottom: "3rem",
+              width: "28rem",
+              transform: "rotate(-10deg)",
+            }}
+          >
+            <Arrow.RightLoopArrow />
+          </span>
+        </div>
+        <div className="relative">
+          <div
+            className="text-4xl uppercase py-6 px-12 border transform rotate-12 absolute rounded-xl"
+            style={{ top: "-5rem", right: "-2rem" }}
+          >
+            Pages
+          </div>
+          <div className="bg-gray flex items-center justify-between py-16 px-32 rounded-2xl">
+            <span className="h-32 block">
+              <PagesIcon />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
