65c566eabfd31d3c1e4d88564d304b836f6ecd95
Sean C Davis <scdavis41@gmail.com>
2022-04-05 09:37:21 -0400

    Fade out intro

--- src/ExplainerVideo/100-Intro/100-Intro.tsx
+++ src/ExplainerVideo/100-Intro/100-Intro.tsx
@@ -1,10 +1,22 @@
+import { interpolate, useCurrentFrame, useVideoConfig } from "remotion"
 import { ContentlayerLogo } from "../../assets"
 
 export const Intro: React.FC = () => {
+  const frame = useCurrentFrame()
+  const { durationInFrames, fps } = useVideoConfig()
+
+  const fadeOutFrames = fps / 2 // 0.5 sec
+  const opacity = interpolate(
+    frame,
+    [durationInFrames - fadeOutFrames, durationInFrames],
+    [1, 0],
+    { extrapolateRight: "clamp" }
+  )
+
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="py-24">
-        <div className="inline-flex items-center">
+        <div className="inline-flex items-center" style={{ opacity }}>
           <div className="h-32 text-primary mr-10">
             <ContentlayerLogo />
           </div>
