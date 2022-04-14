ee297546a8088fcf33b1af2080d8ab2159fbfeea
Sean C Davis <scdavis41@gmail.com>
2022-04-14 15:51:56 -0400

    Contentlayer title

--- src/ExplainerVideo/300-Contentlayer/100-Title.tsx
+++ src/ExplainerVideo/300-Contentlayer/100-Title.tsx
@@ -1,7 +1,24 @@
 import type { SequenceComponent } from "../../types"
+import { useSequenceFade } from "../../hooks"
+import { Logo } from "../../assets"
 
-export const Timeline = {}
+export const Sequence: SequenceComponent = () => {
+  const opacity = useSequenceFade()
 
-export const Sequence: SequenceComponent = ({ timeline }) => {
-  return <p className="text-8xl">300-100-Title</p>
+  return (
+    <div
+      className="w-full h-full flex items-center justify-center"
+      style={{ opacity }}
+    >
+      <div className="py-24">
+        <h3 className="text-5xl font-thin text-center mb-10">Introducing</h3>
+        <div className="inline-flex items-center">
+          <div className="h-32 text-primary mr-10">
+            <Logo.ContentlayerLogo />
+          </div>
+          <h2 className="text-9xl font-bold">Contentlayer</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
